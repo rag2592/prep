41ff76c81399051326d21656005c12f3be184bc7
rag2592 <rsr379@nyu.edu>
2018-02-13 17:06:04 -0500

    Add files via upload

---
+++ Prep/src/findAnagrams.java
@@ -0,0 +1,12 @@
+/**
+ * Created by rsr37 on 2/13/2018.
+ */
+public class findAnagrams {
+    public static void printAnagrams(String s, String file){
+
+    }
+    public static void main(String[] args){
+        if(args.length < 2) System.out.println("Invalid Arguments!");
+        else printAnagrams(args[1],args[0]);
+    }
+}
